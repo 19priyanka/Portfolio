488bbbf5e6991aa4787a3ef8dca0138c32740568
Priyanka Gautam <priyankagautam@Priyankas-MacBook-Pro.local>
2025-01-22 22:56:40 -0700

    test commit for merge protection of main branch

--- src/Projects/ProjectsHomepage.js
+++ src/Projects/ProjectsHomepage.js
@@ -3,7 +3,7 @@ import React from 'react';
  export default function ProjectsHomepage(){
     return (
         <div style={{backgroundColor: 'yellow'}}>
-            <h1>Project homepageinfo</h1>
+            <h1>Project homepageinfo test</h1>
         </div>
     );
 };
